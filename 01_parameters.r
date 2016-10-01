02685ecd8aa0ffecd3df93eeb1ebeab3ca0b67c0
mzh <ma.hed@bluewin.ch>
2016-10-01 21:38:06 +0200

    Parameter values

--- 01_parameters.r
+++ 01_parameters.r
@@ -11,7 +11,7 @@ d5          <- 20000
 treatDoses  <- c(d1, d2, d3, d4, d5)
 genParNames <- "E0,ED50,EMAX"
 genParMean  <- c(1,50,10)
-genParVCov  <- c(.5,30,10)
+genParVCov  <- c(.5,10,2)
 respEqn     <- "E0 + ((DOSE * EMAX)/(DOSE + ED50))"
 respVCov    <- 2
 interimSubj <- ".3, .7"
