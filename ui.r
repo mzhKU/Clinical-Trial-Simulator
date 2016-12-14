98f0c2f8527279b45d76876dea15c29b026e42de
mzh <ma.hed@bluewin.ch>
2016-12-14 22:37:09 +0100

    Added notes, not version controlled

--- ui.r
+++ ui.r
@@ -3,24 +3,28 @@
 shinyUI(fluidPage(
     headerPanel("Clinical Trial Simulator"), 
             fluidRow(
-                column(8, plotOutput("densities"))
+                column(12, plotOutput("densities"))
             ),
             fluidRow(
-                column(2, numericInput("n_patients", "Patients", 500, step=25)),
-                column(2, numericInput("e0", "Baseline", 0, step=2)),
-                column(2, numericInput("ed50", "ED_50",  4, step=10)),
-                column(2, numericInput("emax", "E_max", 10, step=1))
+                column(3, numericInput("n_patients", "Patients", 500, step=25)),
+                column(3, numericInput("e0", "Baseline", 0, step=2)),
+                column(3, numericInput("ed50", "ED_50",  4, step=10)),
+                column(3, numericInput("emax", "E_max", 10, step=1))
             ),
             fluidRow(
-                column(2, numericInput("d1", "Dose 1", 0,  step=5)),
-                column(2, numericInput("d2", "Dose 2", 5,  step=10)),
-                column(2, numericInput("d3", "Dose 3", 10, step=20)),
-                column(2, numericInput("d4", "Dose 4", 40, step=10))
+                column(3, numericInput("d1", "Dose 1", 0,  step=5)),
+                column(3, numericInput("d2", "Dose 2", 5,  step=10)),
+                column(3, numericInput("d3", "Dose 3", 10, step=20)),
+                column(3, numericInput("d4", "Dose 4", 40, step=10))
             ),
             fluidRow(
-                column(2, actionButton("run_simulation", "Start Simulation")),
-                column(2, actionButton("reset", "Clear"))
-            )
+                column(3, actionButton("run_simulation", "Start Simulation"))
+            ),
+            fluidRow(
+                column(3, actionButton("reset", "Clear"))
+            ),
+            br(),
+            p("Developed by M. Hediger")
             # column(1, numericInput("d5", "Dose 5", 80, step=10)),
             #  fluidRow(
             #      column(8,
